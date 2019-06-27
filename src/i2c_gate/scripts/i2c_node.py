d5f19f61586478466d453d16e362bbd1d8e817da
Amir Reza Sadeghi <e.a.sadeqi@gmail.com>
2019-06-27 16:30:44 +0430

    take the modified i2c_node.py

--- src/i2c_gate/scripts/i2c_node.py
+++ src/i2c_gate/scripts/i2c_node.py
@@ -23,14 +23,17 @@ auto_data_bytes = [0x4, 0x4c, 0x4, 0x4c, 0x4, 0x4c] # this is binarized auto_dat
 
 # the call back function that we will use for sending the subscribed data to midware board.
 def callback(data):
-	auto_data = data.auto_com # this is the content of the message [roll pitch throttle]
-
+	#auto_data = data.auto_com # this is the content of the message [roll pitch throttle]
+	auto_data = [data.auto_com.x, data.auto_com.y, data.auto_com.z]
 	# now converting the auto_data to 6 byte for sending via i2c
 	for i in range(6):
 		if (i%2 == 0):
-			auto_data_bytes[i] = ((auto_data[(i//2)] >> 8) & 0xFF)
+			# note that because the Vector3 data type is float we must 
+			# convert the data to integer to able to use bit shifting
+			# operands.
+			auto_data_bytes[i] = ((int(auto_data[(i//2)]) >> 8) & 0xFF)
 		else:
-			auto_data_bytes[i] = auto_data[(i//2)] & 0xFF
+			auto_data_bytes[i] = int(auto_data[(i//2)]) & 0xFF
 
 	# now we will send the bytes via i2c
 	bus.write_i2c_block_data(mwb_add, auto_data_bytes[0], auto_data_bytes[1:])
