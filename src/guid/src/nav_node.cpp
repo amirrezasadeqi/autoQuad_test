6c9b4f8a422ae645be10ff83cd0f749b35f33a2a
Amir Reza Sadeghi <e.a.sadeqi@gmail.com>
2019-09-04 13:29:28 +0200

    adding a part to the navigation node code, to subscribe to optical velocity and use it to send radio values to hold quadrotor in a point if the marker is not detected.

--- src/guid/src/nav_node.cpp
+++ src/guid/src/nav_node.cpp
@@ -5,6 +5,7 @@
 #include <ros/ros.h>
 #include <auto_msgs/auto_com.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <opencv_apps/Point2D.h>
 
 // define global variables for detecting which the marker is lost
 ros::Time ref_time, current_time;
@@ -13,9 +14,11 @@ ros::Duration loss_time, loss_thresh_time(0, 300000000);
 
 // let's define our variables to not forgot. later you can move them another places.
 unsigned short int roll, pitch, throttle;
-double x_r, y_r, z_r, x_c, y_c, z_c;
-// p controller coefficients
+double x_r, y_r, z_r, x_c, y_c, z_c, x_vel, y_vel;
+// p controller coefficients of navigation controller
 float p_roll=20, p_pitch=20, p_throttle=10;
+//p controller coefficients of opticalflow stabilization controller
+float p_vx = -20, p_vy = -20;
 // hover values of roll pitch and throttle
 unsigned short int hov_roll=1503, hov_pitch=1503, hov_throttle=1569, throttle_max=1580, throttle_min=1560;
 
@@ -40,6 +43,14 @@ void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
     z_c = msg->pose.position.z;
 }
 
+//opt_callback
+void opt_callback(const opencv_apps::Point2D::ConstPtr& msg)
+{
+	// reading the velocity which comes from opticalflow
+	x_vel = msg->x;
+	y_vel = msg->y;
+}
+
 // now the main function 
 
 int main(int argc, char **argv)
@@ -49,6 +60,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "Guider_Node");
     //create Node handler for working with this node and manipulate comms of publish and subscribe
     ros::NodeHandle n;
+	//create a subscriber for velocity which comes from opticalflow
+	ros::Subscriber optflow_sub = n.subscribe("/optical_velocity", 10, opt_callback);
     //create Two subscriber, one for refrence point and one for current point
     ros::Subscriber ref_sub = n.subscribe("ref_pose", 1000, refCallback);
     ros::Subscriber pose_sub = n.subscribe("/aruco_single/pose", 1000, poseCallback);
@@ -117,8 +130,40 @@ int main(int argc, char **argv)
 	else
 	{
 		// this is for when the marker is lost and we must quadrotor stop at the point
-		roll = hov_roll;
-		pitch = hov_pitch;
+		//roll = hov_roll;
+		//pitch = hov_pitch;
+		//throttle = hov_throttle;
+
+		// let's test for using the opticalflow and holding quadrotor at a point
+		//roll
+		roll = p_vx * (-x_vel);
+		if (roll > 10){
+			roll = 10;
+		}
+		else if (roll < -10){
+			roll = -10;
+		}
+		else{
+			// do nothing
+		}
+		//roll value to send mid-ware board
+		roll = hov_roll + roll;
+		//pitch
+		pitch = p_vy * (-y_vel);
+		if (pitch > 10){
+			pitch = 10;
+		}
+		else if (pitch < -10){
+			pitch = -10;
+		}
+		else{
+			//do nothing
+		}
+		//pitch value to send mid-ware board
+		pitch = hov_pitch + pitch;	
+		//because we use altitude hold mode of flight we use the hover
+		//value of throttle to hode the alltitude. in future we use this 
+		//with p controller and the data of ultrasonic sensor.
 		throttle = hov_throttle;
 	}
 	
